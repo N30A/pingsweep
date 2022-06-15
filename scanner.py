cc18fbbfba66d483e97dc17944958d7492031f39
0xrytlock <neo.ahlander@protonmail.com>
2022-06-15 22:22:19 +0200

    more small improvement

--- scanner.py
+++ scanner.py
@@ -8,7 +8,7 @@ from threading import Thread
 from pythonping import ping
 
 
-def scan(success, failed):
+def scan(queue, success, failed):
     while True:
         ip = queue.get()
         response = ping(ip, count=1, timeout=0.2)
@@ -23,6 +23,7 @@ def scan(success, failed):
 
 
 def main():
+    queue = Queue()
     parser = ArgumentParser()
     parser.add_argument("IP_RANGE", help="ip range of the network, e.g. 192.168.1.0/24")
     parser.add_argument(
@@ -31,19 +32,19 @@ def main():
         action="store_true",
         help="increase output verbosity",
     )
-
     args = parser.parse_args()
 
     success = []
     failed = []
-
-    start = datetime.now().replace(microsecond=0)
-
-    for _ in range(cpu_count() * 10):
-        Thread(target=scan, args=(success, failed), daemon=True).start()
+    workers = cpu_count() * 10
 
     for ip in IPv4Network(args.IP_RANGE).hosts():
         queue.put(ip.exploded)
+
+    start = datetime.now().replace(microsecond=0)
+
+    for _ in range(workers):
+        Thread(target=scan, args=(queue, success, failed), daemon=True).start()
 
     queue.join()
 
@@ -57,5 +58,4 @@ def main():
 
 
 if __name__ == "__main__":
-    queue = Queue()
     main()
