811e210bec71c2ae46458c38f15c95094e0156e9
0xrytlock <neo.ahlander@protonmail.com>
2022-06-18 17:44:44 +0200

    Functionality upgrade, more arguments, fixes

--- scanner.py
+++ scanner.py
@@ -8,52 +8,85 @@ from threading import Thread
 from pythonping import ping
 
 
-def scan(queue, success, failed):
+def scan(queue: Queue, wait: float, success: list, failed: list) -> None:
     while True:
-        ip = queue.get()
-        response = ping(ip, count=1, timeout=0.2)
+        address = queue.get()
+        response = ping(address, count=1, timeout=wait)
 
         if response.success():
-            success.append(ip)
-            print(ip)
+            success.append(address)
+            print(address)
         else:
-            failed.append(ip)
+            failed.append(address)
 
         queue.task_done()
 
 
 def main():
+    threads = cpu_count()
+    success = []
+    failed = []
+
     queue = Queue()
     parser = ArgumentParser()
-    parser.add_argument("IP_RANGE", help="ip range of the network, e.g. 192.168.1.0/24")
+    parser.add_argument("ip_range", help="ip range of the network, e.g. 192.168.1.0/24")
     parser.add_argument(
-        "-v",
-        "--verbose",
+        "-q",
+        "--quiet",
         action="store_true",
-        help="increase output verbosity",
+        help="quiet output, only print ip-addresses to the screen",
+    )
+    parser.add_argument(
+        "-w",
+        "--wait",
+        type=float,
+        metavar="sec",
+        default=0.1,
+        help="timeout in seconds to wait for each reply (default: 0.1s)",
+    )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        type=int,
+        metavar="int",
+        default=threads,
+        help=f"number of concurrent threads (default: {threads})",
     )
     args = parser.parse_args()
 
-    success = []
-    failed = []
-    workers = cpu_count() * 10
+    # Append every valid address to the queue from the user specifed ip range.
+    for address in IPv4Network(args.ip_range).hosts():
+        queue.put(address.exploded)
 
-    for ip in IPv4Network(args.IP_RANGE).hosts():
-        queue.put(ip.exploded)
+    if not args.quiet:
+        print(
+            f"\n> IP Range: {args.ip_range}",
+            f"\n> Threads: {threads}",
+            f"\n> Quiet: {args.quiet}",
+            f"\n> Wait: {args.wait}s\n",
+        )
 
+    # Time snapshot
     start = datetime.now().replace(microsecond=0)
 
-    for _ in range(workers):
-        Thread(target=scan, args=(queue, success, failed), daemon=True).start()
+    # Start the "scan" function with the amount of threads specifed.
+    for _ in range(args.threads):
+        Thread(
+            target=scan,
+            args=(queue, args.wait, success, failed),
+            daemon=True,
+        ).start()
 
+    # Wait until all work is finished before continuing.
     queue.join()
 
+    # Time snapshot
     end = datetime.now().replace(microsecond=0)
 
-    if args.verbose:
+    if not args.quiet:
         print(
             f"\nFinished in: {end - start} |",
-            f"Success: {len(success)} Failed: {len(failed)}",
+            f"Success: {len(success)}, Failed: {len(failed)}, Total: {len(success) + len(failed)}",
         )
 
 
